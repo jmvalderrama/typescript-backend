32ec316f4b739b371ba34b6b35735b12bc26ccba
Juan Mario Valderrama <juancho9402@hotmail.com>
2019-11-12 17:02:48 -0500

    Products endpoint HTTP  method changed to GET

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -3,5 +3,9 @@ import Product, { IProduct } from '../models/Product';
 
 export const getProducts = async (req: Request, res: Response) => {
   const products: IProduct[] = await Product.find({});
-  res.status(200).send(products);
+  res.status(200).send({
+    code: 'ok',
+    message: 'Productos consultados satisfactoriamente.',
+    data: products
+  });
 }
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -4,6 +4,6 @@ import { getProducts } from '../controllers/product.controller';
 
 const productRouter: Router = Router();
 
-productRouter.route('/').post(getProducts);
+productRouter.route('/').get(getProducts);
 
 export default productRouter;
